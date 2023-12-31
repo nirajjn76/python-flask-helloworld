45fcd0394d05e401f09c72ac0ec8c23bead08469
nirajjn76 <83336437+nirajjn76@users.noreply.github.com>
2023-12-31 11:54:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello World!"
+    return "Hello World from docker container!"
 if __name__ == "__main__":
-    app.run()
+    app.run('0.0.0.0', '8081')
