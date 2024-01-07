361dd03ffd617efd7ba8227ee6c8839d06100826
user <nirajjain738@gmail.com>
2024-01-07 11:56:12 +0530

    modified app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Hello World from docker container!"
+    return "Hello World from docker container from pipeline!"
 if __name__ == "__main__":
     app.run('0.0.0.0')
